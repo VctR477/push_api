e9ca0109851af4e3f23c70c0e2689ed1ad1e8ffe
Viktor Klimov <VKKlimov@alfabank.ru>
2025-06-02 22:40:14 +0300

    fast commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(async function() {
+(function() {
     const checkPermission = () => {
         if (!('serviceWorker' in navigator)) {
             throw new Error('Браузер не поддерживает Service Worker API');
@@ -25,10 +25,22 @@
         }
     };
 
-    document.getElementById('btn').addEventListener('click', requestNotificationPermission);
+    const main = async () => {
+        checkPermission();
+        await requestNotificationPermission();
+        await registerSW();
+    };
 
-    checkPermission();
-    const reg = await registerSW();
-    // reg.showNotification('Hello w234rld');
-    // requestNotificationPermission();
+    document.getElementById('btn').addEventListener('click', main);
+
+    // document.getElementById('btn-2').addEventListener('click', function() {
+    //     if (reg.showNotification) {
+    //         reg.showNotification('Привет Мир!', {
+    //             body: "Какой-то там текст ла-ла-ла-лалалала!",
+    //             // icon: "https://sun9-34.userapi.com/s/v1/ig2/jgonCQJcm9ffBM8mFBS9FxOPaGrqO9L1noRkX_gLU_i_59JqGVOdm-SAbb2LHO_76iW3LcS19Lh7juslMn8RZbpy.jpg?quality=95&crop=512,1204,336,336&as=32x32,48x48,72x72,108x108,160x160,240x240&ava=1&cs=50x50",
+    //             icon: "https://sun9-68.userapi.com/impg/BDHgqHyZAnJnvw5yCj6ra1Zw85CoDi1a8lEWsw/FG86880mpWA.jpg?size=1620x2160&quality=95&sign=c058a227063facfe2030b16d4e079cd6&type=album",
+    //             vibrate: [200, 100, 200, 100, 200, 100, 200],
+    //         });
+    //     }
+    // });
 })();
--- sw.js
+++ sw.js
@@ -1,1 +1,36 @@
-console.log('hello from service worker');
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/')
+  ;
+  const rawData = atob(base64);
+  return Uint8Array.from([...rawData].map((char) => char.charCodeAt(0)));
+}
+
+async function saveSubscription(subscription) {
+    const response = await fetch('http://localhost:3000/save-subscription', {
+        method: 'post',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(subscription),
+    });
+    const result = response.json();
+
+    return result;
+}
+
+self.addEventListener('activate', async () => {
+    const subscription = await self.registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array('BEiyKu8QL3LsskG70KRqN2B8Xg4EkfzuoAoWQSqmVdyUut45FoAW5GwQMuH3HSVdaHlRTKuIscSfbM3OVsjaS_o'),
+    });
+
+    const response = await saveSubscription(subscription);
+    console.log(response);
+});
+
+// Public Key:
+// BEiyKu8QL3LsskG70KRqN2B8Xg4EkfzuoAoWQSqmVdyUut45FoAW5GwQMuH3HSVdaHlRTKuIscSfbM3OVsjaS_o
+
+// Private Key:
+// xX04Xud5mbxs9coXo6PzaUFQJQZMD3d3S2vWsCzvBNE
